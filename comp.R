48896218be60f6df5a6dd5c1fedf90276d9549fc
Gefasa13 <zhenweng@bu.edu>
2024-12-16 17:13:00 -0500

    Update comp.R

--- comp.R
+++ comp.R
@@ -13,14 +13,10 @@ df_filtered <- comp %>%
 df_filtered <- df_filtered %>%
   mutate(across(starts_with("X202"), as.numeric))
 
-# 将数据从宽格式转为长格式，并提取年份信息
 df_long <- df_filtered %>%
   pivot_longer(
     cols = starts_with("X202"),
     names_to = "year",
     values_to = "value"
   ) %>%
-  # 提取year列中的4位年份
-  mutate(year = as.numeric(str_extract(year, "\\d{4}")))
-
-write_csv(df_long,"comp2.csv")
+mutate(year = as.numeric(str_extract(year, "\\d{4}")))
